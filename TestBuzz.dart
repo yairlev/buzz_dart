150e71f06b5041032f715cd87e5fb17a0250ad39
Yoad Snapir <yoadsn@gmail.com>
2012-04-27 17:07:36 +0300

    Implement Buzz partially

--- TestBuzz.dart
+++ TestBuzz.dart
@@ -1,4 +1,5 @@
 #import('dart:html');
+#import('dart:core');
 #source('Sound.dart');
 #source('Buzz.dart');
 
@@ -7,7 +8,22 @@ void main() {
   /*
   * Tests for the Buzz class
   */
-  print("IsSupported: " + Buzz.isSupported);
+  print("IsSupported: " + Buzz.Instance.isSupported);
+  print("isOGGSupported: " + Buzz.Instance.isOGGSupported);
+  print("isWAVSupported: " + Buzz.Instance.isWAVSupported);
+  print("isMP3Supported: " + Buzz.Instance.isMP3Supported);
+  print("isAACSupported: " + Buzz.Instance.isAACSupported);
+  
+  print("toTime(3601,true): ${Buzz.Instance.toTimer(3601, true)}");
+  print("toTime(61,false): ${Buzz.Instance.toTimer(61, false)}");
+  print("toTime(31,false): ${Buzz.Instance.toTimer(31, false)}");
+  print("toTime(0,false): ${Buzz.Instance.toTimer(0, false)}");
+  
+  print("fromTimer(01:00): ${Buzz.Instance.fromTimer("01:00")}");
+  print("fromTimer(00:00): ${Buzz.Instance.fromTimer("00:00")}");
+  print("fromTimer(01:01:00): ${Buzz.Instance.fromTimer("01:01:00")}");
+  print("fromTimer(00:01:00): ${Buzz.Instance.fromTimer("00:01:00")}");
+  print("fromTimer(59:59): ${Buzz.Instance.fromTimer("59:59")}");
   
   
   /*
